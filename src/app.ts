393eb439880efe82816e0001a82be019c3366819
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2025-07-28 20:03:59 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import cors from "cors";
+import express, { Request, Response } from "express";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+import { router } from "./app/routes";
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use("/api/v1", router)
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        message: "Welcome to Digital Wallet Management System Backend"
+    })
+})
+
+
+app.use(globalErrorHandler)
+
+app.use(notFound)
+
+export default app
