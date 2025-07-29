505cf08c6138708e4c4b974976821dbe54d08cce
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2025-07-29 15:04:15 +0600

    user login completed

---
+++ src/app/modules/auth/auth.controller.ts
@@ -0,0 +1,35 @@
+import { catchAsync } from "../../utils/catchAsync";
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status-codes";
+import { sendResponse } from "../../utils/sendResponse";
+import { setAuthCookie } from "../../utils/setCookie";
+import { AuthServices } from "./auth.service";
+
+const credentialsLogin = catchAsync(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async (req: Request, res: Response, next: NextFunction) => {
+    const loginInfo = await AuthServices.credentialsLogin(req.body);
+
+    // // res.cookie("accessToken", loginInfo.accessToken, {
+    // //     httpOnly: true,
+    // //     secure: false
+    // // })
+
+    // // res.cookie("refreshToken", loginInfo.refreshToken, {
+    // //     httpOnly: true,
+    // //     secure: false,
+    // // })
+
+    setAuthCookie(res, loginInfo);
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "User Logged In Successfully",
+      data: loginInfo,
+    });
+  }
+);
+
+export const AuthControllers = {
+  credentialsLogin,
+};
---
+++ src/app/modules/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import AppError from "../../errorHelpers/AppError";
+import { IUser } from "../user/user.interface";
+import { User } from "../user/user.model";
+import httpStatus from "http-status-codes";
+import bcryptjs from "bcryptjs";
+import { createUserTokens } from "../../utils/userTokens";
+
+const credentialsLogin = async (payload: Partial<IUser>) => {
+  const { email, password } = payload;
+
+  const isUserExist = await User.findOne({ email });
+
+  if (!isUserExist) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Email does not exist");
+  }
+
+  const isPasswordMatched = await bcryptjs.compare(
+    password as string,
+    isUserExist.password as string
+  );
+
+  if (!isPasswordMatched) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Incorrect Password");
+  }
+
+  const userTokens = createUserTokens(isUserExist);
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { password: pass, ...rest } = isUserExist.toObject();
+
+  return {
+    accessToken: userTokens.accessToken,
+    refreshToken: userTokens.refreshToken,
+    user: rest,
+  };
+};
+
+export const AuthServices = {
+  credentialsLogin,
+};
